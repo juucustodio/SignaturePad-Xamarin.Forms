9c04f3dda84427778963f405f8e9ee593cb71cbb
Juliano Custódio <juliano.custodio@hotmail.com.br>
2018-01-11 13:06:12 -0200

    Implement SignaturePad

---
+++ SignaturePadDemo/SignaturePadDemo/SignaturePadDemo/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using SignaturePad.Forms;
+using Xamarin.Forms;
+
+namespace SignaturePadDemo
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        public async void Save(object sender, EventArgs eventArgs)
+	    {
+
+	        Stream stream = await Pad.GetImageStreamAsync(SignatureImageFormat.Jpeg);
+        }
+	}
+}
